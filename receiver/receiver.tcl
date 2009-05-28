1303081e3bba9dfb72e12a65eb2155e0cdf53015
David McNett <nugget@yankee.hou.flightaware.com>
2009-05-28 10:29:06 -0500

    Rename "committer" to "receiver"

--- receiver/receiver.tcl
+++ receiver/receiver.tcl
@@ -10,17 +10,24 @@ array set DB {
 	password        password
 	connect_timeout 5
 }
+proc epoch_ts {epoch} {
+	return "(SELECT TIMESTAMP WITH TIME ZONE 'epoch' + $epoch * INTERVAL '1 second')"
+}
 
 if {[catch {set db [pg_connect -connlist [array get DB]]} result] == 1} {
 	puts "Unable to connect to database: $result"
-} else {
-	while {[gets stdin line] >= 0} {
-		if {[regexp {^([A-Z]+) (.*)$} $line _ key value]} {
-			set cdata($key) "$value"
-		}
+	exit
+}
+while {[gets stdin line] >= 0} {
+	if {[regexp {^([A-Z]+) (.*)$} $line _ key value]} {
+		set cdata($key) "$value"
 	}
 
-	set res [pg_exec $db "INSERT INTO commits (hash, tree_hash, parent_hash,
+	puts $line
+	if {$line == ""} {
+		puts "Inserting $cdata(hash)"
+
+		set res [pg_exec $db "INSERT INTO commits (hash, tree_hash, parent_hash,
                                           author_name,author_email,author_date,
 	                                  commit_name,commit_email,commit_date,
 	                                  subject,body)
@@ -31,16 +38,20 @@ if {[catch {set db [pg_connect -connlist [array get DB]]} result] == 1} {
 	                [pg_quote $cdata(AUTHORNAME)],
 	                [pg_quote $cdata(AUTHOREMAIL)],
 	                [epoch_ts $cdata(AUTHORDATE)],
-	                [pg_quote $cdata(COMMITNAME)],
-	                [pg_quote $cdata(COMMITEMAIL)],
-	                [epoch_ts $cdata(COMMITDATE)],
+	                [pg_quote $cdata(COMMITERNAME)],
+	                [pg_quote $cdata(COMMITEREMAIL)],
+	                [epoch_ts $cdata(COMMITERDATE)],
 	                [pg_quote $cdata(SUBJECT)],
 	                [pg_quote $cdata(BODY)]
-	             WHERE hash NOT IN (SELECT DISTINCT hash FROM commits);"]
-	puts [pg_result -error $res]
-	pg_result -clear $res
+	             WHERE [pg_quote $cdata(HASH)] NOT IN (SELECT DISTINCT hash FROM commits);"]
 
-	set res [pg_exec $db "INSERT INTO commit_branch (hash,branch) VALUES ([pg_quote $cdata(HASH)], [pg_quote $cdata(BRANCH)]);"
-	puts [pg_result -error $res]
-	pg_result -clear $res
+		puts [pg_result $res -error]
+		pg_result $res -clear
+	
+		set res [pg_exec $db "INSERT INTO commit_branch (hash,branch) VALUES ([pg_quote $cdata(HASH)], [pg_quote $cdata(BRANCH)]);"]
+		puts [pg_result $res -error]
+		pg_result $res -clear
+
+		unset -nocomplain cdata
+	}
 }
